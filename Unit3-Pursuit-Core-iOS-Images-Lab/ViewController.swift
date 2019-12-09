9eb2df34d74fd8f7772ecf58e7d0f4d66b08b2fb
LubaKaper <liubovkaper@pursuit.com>
2019-12-09 17:46:30 -0500

    Initial Commit

---
+++ Unit3-Pursuit-Core-iOS-Images-Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Unit3-Pursuit-Core-iOS-Images-Lab
+//
+//  Created by Liubov Kaper  on 12/9/19.
+//  Copyright © 2019 Luba Kaper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
